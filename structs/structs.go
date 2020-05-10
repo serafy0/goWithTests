dc5b71d32ff3590d440e7b84e13a6b2b298d8306
mohamedelserafy <elserafy02@gmail.com>
2020-05-10 04:11:52 +0200

    Area

---
+++ structs/structs.go
@@ -0,0 +1,24 @@
+package structs
+
+import "math"
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.width * rectangle.height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * math.Pi * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return (2 * (rectangle.width + rectangle.height))
+}
